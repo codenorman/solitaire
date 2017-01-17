88222bd62a5e307991a74b37fccb7e6bf8a0bdbf
Brian Chirgwin <C:\Users\User\Documents\Bat!>
2017-01-16 20:33:28 -0600

    added tests, isBlack, isRed

--- src/gameEngine.js
+++ src/gameEngine.js
@@ -1,5 +1,10 @@
 var _ = require('lodash');
 
+// shuffle
+// isValidMove
+// validMoves
+//
+
 module.exports = {
   genDeck: function (pack) {
     return _.reduce(pack, function (a, b) {
@@ -9,6 +14,12 @@ module.exports = {
         })
       }))
     })
+  },
+  isBlack: function(card){
+    return card[1] === 's' || card[1] === 'c'
+  },
+  isRed: function(card){
+    return !this.isBlack(card)
   }
 };
 
