13ac8116d867fddc0a784e95f4b52759da42898c
Brian Chirgwin <C:\Users\User\Documents\Bat!>
2017-01-16 19:27:06 -0600

    remove unused

--- src/gameEngine.js
+++ src/gameEngine.js
@@ -1,6 +1,4 @@
 var _ = require('lodash');
-var card = require('./card');
-var cards = require('./cards');
 var config = require('./games/klondike.json');
 
 var genDeck = function (pack) {
@@ -12,22 +10,5 @@ var genDeck = function (pack) {
     }))
   })
 };
-
-var setUp = function(){
-
-};
-
-var start = function(){
-
-};
-
-var isValidMove = function (currentState, newState) {
-
-};
-
-var isGameWon = function (boardState) {
-
-};
-
 var deck = genDeck(config.pack)
 console.log(deck);
