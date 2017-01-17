e4a708f4e3b2b2b4c4fe6e18640f6c4e7ae353b7
Brian Chirgwin <C:\Users\User\Documents\Bat!>
2017-01-16 20:05:26 -0600

    second test passes

--- spec/gameEngine.spec.js
+++ spec/gameEngine.spec.js
@@ -4,5 +4,10 @@ describe('gen Deck', function(){
 
   it('should generate a deck of cards',function(){
     expect(ge.genDeck(["a","cdhs"])).toEqual(['ac','ad','ah','as']);
-  })
+  });
+
+  it('should generate a deck of A and 2',function(){
+    expect(ge.genDeck(["a2","cdhs"])).toEqual(['ac','ad','ah','as','2c','2d','2h','2s']);
+  });
+
 });
