1ad7180c4bd19f1b1ac051cbf635c65ab08ee249
Brian Chirgwin <C:\Users\User\Documents\Bat!>
2017-01-16 20:01:40 -0600

    first test passes

--- spec/gameEngine.spec.js
+++ spec/gameEngine.spec.js
@@ -3,6 +3,6 @@ var ge = require('../src/gameEngine');
 describe('gen Deck', function(){
 
   it('should generate a deck of cards',function(){
-    expect(ge.genDeck(["a","cdhs"])).toEqual('ac,ad,as,ah');
+    expect(ge.genDeck(["a","cdhs"])).toEqual(['ac','ad','ah','as']);
   })
 });
